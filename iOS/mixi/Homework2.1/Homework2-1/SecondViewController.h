fb563634f9b605cb3bb10230712102b01b180a4e
濱岡　俊介 <01014532@CA3591.local>
2018-12-03 13:39:16 +0900

    Homework fix

---
+++ iOS/mixi/Homework2.1/Homework2-1/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Homework2-1
+//
+//  Created by 濱岡　俊介 on 2018/11/21.
+//  Copyright © 2018年 濱岡　俊介. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
