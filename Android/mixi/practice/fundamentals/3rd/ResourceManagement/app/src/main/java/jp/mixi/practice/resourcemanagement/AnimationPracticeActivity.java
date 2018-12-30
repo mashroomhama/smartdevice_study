636bb62d51c7c15a7434e55df75c50a0ec4e2ad2
濱岡俊介 <shunsuke@hamaokashunsukenoMacBook-Pro.local>
2018-12-30 18:39:46 +0900

    mixi tutorial of android version

---
+++ Android/mixi/practice/fundamentals/3rd/ResourceManagement/app/src/main/java/jp/mixi/practice/resourcemanagement/AnimationPracticeActivity.java
@@ -0,0 +1,15 @@
+package jp.mixi.practice.resourcemanagement;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by takafumi.nanao on 2015/02/05.
+ */
+public class AnimationPracticeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.animation_practice);
+    }
+}
---
+++ Android/mixi/practice/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/practice/messagingandnotification/NotificationActivity.java
@@ -0,0 +1,18 @@
+package jp.mixi.practice.messagingandnotification;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by suino on 2015/02/26.
+ */
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+        // TODO ここで通知を表示する
+    }
+}
